14c9fa19f9fa18297da2b76269e47fdd0f81738e
Daviesss <ogunsdavis53@gmail.com>
2023-04-16 14:30:30 +0100

    .....c++

--- move/src/pointers.cpp
+++ move/src/pointers.cpp
@@ -16,7 +16,11 @@ public:
       x = 2;
       int* c = &x;
       *c = 99;
-      cout << "The pointer is:" << c << endl;
+      for (int i = 0;i<sizeof(c);i++){
+         cout << "The pointer is:" << c <<endl;
+      }
+
+     //cout << "The pointer is:" << c << endl;
     }
 
 };
