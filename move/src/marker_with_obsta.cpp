86dabd6eda0ea601ab20ff69ba7e71f1966f08dc
Daviesss <ogunsdavis53@gmail.com>
2023-04-17 22:51:58 +0100

    .....c++

--- move/src/marker_with_obsta.cpp
+++ move/src/marker_with_obsta.cpp
@@ -28,7 +28,7 @@ class action{
         velocity_pub.publish(velocity_messsage);
       }
 
-      //Publish to Visual marker method
+      //Publish the Visual marker method
       void read_marker(){
         markers_.header.frame_id = "lidar_1"; //start transition from the laser_frame
         markers_.ns = "robot_namespace";
@@ -58,18 +58,16 @@ class action{
           cout << "The position of markers(x,y,z):" <<  markers_.pose.position.x << " " <<   markers_.pose.position.y  << " "<<   markers_.pose.position.z << endl;
           ROS_INFO("The Marker node has started.....");
         }
-
       }
 };//main class action
 
 void laser_callback(const sensor_msgs::LaserScanConstPtr& msg){
     action p;
     p.forward();//robot moves forward
-    action p;
-    p.read_marker();
+    //action store(p);//copy constructor
     float angle_min = msg->angle_min;
     float angle_max = msg->angle_max;
-    float angle_increment = msg->angle_increment;
+    float angle_increment = msg->angle_increment; 
     float range_min = msg->range_min;
     float range_max = msg->range_max;
     std::vector<float> ranges = msg->ranges;
@@ -103,8 +101,8 @@ int main(int argc,char**argv){
     ros::init(argc,argv,"Visual_marker_node");
     ros::NodeHandle nh;
     ros::Subscriber laser_scan = nh.subscribe("/scan",1,laser_callback);
-    // action p;
-    // p.read_marker();
+    action p;
+    p.read_marker();
     ros::spin();
     return 0;
 }
